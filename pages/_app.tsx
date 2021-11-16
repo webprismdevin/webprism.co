40c563ceaed176dd4b9fbe8bd3170ca88fe04b78
Devin Santamaria <devin@webprism.co>
2021-11-16 01:16:06 -0800

    tiny doot

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -71,6 +71,10 @@ function CustomApp({ Component, pageProps }: AppProps) {
     }
   }, [])
 
+  useEffect(() => {
+    process.browser && document.querySelector(".bm-menu")?.setAttribute("background-image", "url(/menu_img_5.webp)")
+  }, [])
+
   return (
     <>
       <Head>
