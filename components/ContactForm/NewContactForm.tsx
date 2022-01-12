263dfcd7c9c2e346f5a31a28811de27ea3ed7ac3
Devin Santamaria <devin@webprism.co>
2022-01-11 21:12:46 -0800

    Update NewContactForm.tsx

--- components/ContactForm/NewContactForm.tsx
+++ components/ContactForm/NewContactForm.tsx
@@ -9,40 +9,76 @@ import {
   Textarea,
   Input,
   FormControl,
-  FormLabel
+  FormLabel,
 } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
+import { useFormik } from "formik";
 
 export default function ContactForm() {
-  const [fName, setFName] = useState("");
-  const [lName, setLName] = useState("");
-  const [phone, setPhone] = useState("");
-
-  const fNameError = fName === ''
+  const formik = useFormik({
+    initialValues: {
+      first_name: "",
+      last_name: "",
+      phone: "",
+      email: "",
+      message: "",
+    },
+    onSubmit: async (values) => {
+      await new Promise((r) => setTimeout(r, 500));
+      alert(JSON.stringify(values, null, 2));
+      formik.setSubmitting(false);
+    },
+  });
 
   return (
-    <SimpleGrid templateColumns={`repeat(2, 1fr)`} gap={6} w="full">
-      <GridItem colSpan={[2, 1]}>
-        <FormControl isInvalid={fNameError} isRequired>
-          <FormLabel htmlFor="first_name">First Name</FormLabel>
-          <Input id="first_name" placeholder="Enter your first name" value={fName} onChange={e => setFName(e.target.value)}/>
-        </FormControl>
-      </GridItem>
-      <GridItem colSpan={[2, 1]}>
-        <Input placeholder="last name" />
-      </GridItem>
-      <GridItem colSpan={[2]}>
-        <Input placeholder="phone number" />
-      </GridItem>
-      <GridItem colSpan={[2]}>
-        <Input placeholder="email address" />
-      </GridItem>
-      <GridItem colSpan={[2]}>
-        <Textarea placeholder="your message" rows={5} />
-      </GridItem>
-      <GridItem>
-        <Button>Submit ✓</Button>
-      </GridItem>
-    </SimpleGrid>
+    <form onSubmit={formik.handleSubmit}>
+      <SimpleGrid templateColumns={`repeat(2, 1fr)`} gap={6} w="full">
+        <GridItem colSpan={[2, 1]}>
+          <Input
+            placeholder="Enter your first name"
+            name="first_name"
+            onChange={formik.handleChange}
+            value={formik.values.first_name}
+          />
+        </GridItem>
+        <GridItem colSpan={[2, 1]}>
+          <Input
+            placeholder="last name"
+            name="last_name"
+            onChange={formik.handleChange}
+            value={formik.values.last_name}
+          />
+        </GridItem>
+        <GridItem colSpan={[2]}>
+          <Input
+            placeholder="phone number"
+            name="phone"
+            onChange={formik.handleChange}
+            value={formik.values.phone}
+          />
+        </GridItem>
+        <GridItem colSpan={[2]}>
+          <Input
+            name="email"
+            type="email"
+            placeholder="email address"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+        </GridItem>
+        <GridItem colSpan={[2]}>
+          <Textarea
+            placeholder="your message"
+            rows={5}
+            name="message"
+            onChange={formik.handleChange}
+            value={formik.values.message}
+          />
+        </GridItem>
+        <GridItem>
+          <Button isLoading={formik.isSubmitting} type="submit" loadingText="Submitting...">Submit ✓</Button>
+        </GridItem>
+      </SimpleGrid>
+    </form>
   );
 }
