04e436211555f95b826cadd98367e8b2c4b985b9
Devin Santamaria <devin@webprism.co>
2022-02-14 21:18:50 -0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -89,16 +89,12 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <Footer />
         <MailingList />
         <ColorModeScript initialColorMode={customTheme.initialColorMode} />
-        <Script
-          id="segment"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="pY5WgoDP09niot4j8hMVyCkwnp4qy0dz";;analytics.SNIPPET_VERSION="4.15.3";
-            analytics.load("pY5WgoDP09niot4j8hMVyCkwnp4qy0dz");
-            analytics.page();
-            }}();`,
-          }}
-        />
+        <Script id="segment" strategy="beforeInteractive">{`
+          !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="pY5WgoDP09niot4j8hMVyCkwnp4qy0dz";;analytics.SNIPPET_VERSION="4.15.3";
+          analytics.load("pY5WgoDP09niot4j8hMVyCkwnp4qy0dz");
+          analytics.page();
+          }}();
+        `}</Script>
       </ChakraProvider>
     </>
   );
